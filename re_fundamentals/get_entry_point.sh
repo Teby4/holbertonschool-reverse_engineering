8be6af4804446c98e47c9e5876b1c69f07bc95fc
Teby4 <felipeoliveramantero@gmail.com>
2025-03-08 07:26:07 -0500

    ¨task¨

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -14,9 +14,9 @@ if ! file "$file_name" | grep -q "ELF"; then
     exit 1
 fi
 
-magic_number=$(readelf -h $file_name | grep Magic: | awk '{$1="";print}')
-class=$(readelf -h $file_name | grep Class: | awk '{$1="";print}')
-byte_order=$(readelf -h $file_name | grep Data: | awk '{$1="";$2="";$3="";print}')
-entry_point_address=$(readelf -h $file_name | grep "Entry point address:" | awk '{$1="";$2="";$3="";print}')
+magic_number=$(readelf -h $file_name | grep Magic: | awk '{ $1=""; sub(/^ /, ""); print }')
+class=$(readelf -h $file_name | grep Class: | awk '{ $1=""; sub(/^ /, ""); print }')
+byte_order=$(readelf -h $file_name | grep Data: | awk '{$1=$2=$3=""; sub(/^(\s*)/, ""); print}')
+entry_point_address=$(readelf -h $file_name | grep "Entry point address:" | awk '{print $4}')
 
 display_elf_header_info
